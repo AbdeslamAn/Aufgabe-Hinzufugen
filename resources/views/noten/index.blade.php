bb871732be53d610727687bcdeb03db49fef832a
Abdeslam Anssar <abdeslamanssar@gmail.com>
2025-06-15 19:02:21 +0000

    Alles Noten zeigen

---
+++ resources/views/noten/index.blade.php
@@ -0,0 +1,23 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Noten
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            @forelse ($aufgabes as $aufgabe)
+            <div class="bg-white p-6 overflow-hidden shadow-sm sm:rounded-lg">
+                        <h2 class="font-bold  text-2xl text-indigo-600">{{ $aufgabe->title }}</h2>
+                        <p class="mt-2 ">{{Str::limit($aufgabe->text, 200, '...') }}</p> 
+                        <span class="block mt-4 text-sm opacity-70">{{ $aufgabe->updated_at->diffForHumans()}}</span>
+            </div>
+            @empty
+            <p>Sie haben keine Noten.</p>
+            @endforelse
+            {{$aufgabes->links()}}
+
+        </div>
+    </div>
+</x-app-layout>
