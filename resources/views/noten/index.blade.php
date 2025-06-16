a90ab572468bea6d0ab7cb02f60925887a34b8ce
Abdeslam Anssar <abdeslamanssar@gmail.com>
2025-06-16 19:27:16 +0000

    Link-button +neu Note erstellt / Create link button +neu Note

--- resources/views/noten/index.blade.php
+++ resources/views/noten/index.blade.php
@@ -7,11 +7,15 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <x-link-button href="{{ route('aufgabes.create')}}">
+                + Neu Note
+            </x-link-button>
             @forelse ($aufgabes as $aufgabe)
             <div class="bg-white p-6 overflow-hidden shadow-sm sm:rounded-lg">
-                        <h2 class="font-bold  text-2xl text-indigo-600">{{ $aufgabe->title }}</h2>
-                        <p class="mt-2 ">{{Str::limit($aufgabe->text, 200, '...') }}</p> 
-                        <span class="block mt-4 text-sm opacity-70">{{ $aufgabe->updated_at->diffForHumans()}}</span>
+
+                <h2 class="font-bold  text-2xl text-indigo-600">{{ $aufgabe->title }}</h2>
+                <p class="mt-2 ">{{Str::limit($aufgabe->text, 200, '...') }}</p>
+                <span class="block mt-4 text-sm opacity-70">{{ $aufgabe->updated_at->diffForHumans()}}</span>
             </div>
             @empty
             <p>Sie haben keine Noten.</p>
