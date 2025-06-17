267145811013055d305db0a2872ef1f784dd9a29
Abdeslam Anssar <abdeslamanssar@gmail.com>
2025-06-17 18:15:11 +0000

    Button Notiz bearbeitet hinzufügen / Add Button Notiz bearbeitet

--- resources/views/noten/show.blade.php
+++ resources/views/noten/show.blade.php
@@ -7,10 +7,12 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            
+
             <div class="flex gap-6">
                 <p class="opacity-70"><strong>Erstelen : </strong> {{$aufgabe->created_at->diffForHumans()}}</p>
                 <p class="opacity-70"><strong>Letzte ändern :</strong> {{$aufgabe->updated_at->diffForHumans()}}</p>
+
+                <x-link-button href="{{ route('aufgabes.edit', $aufgabe) }}" class="ml-auto">Notiz bearbeiten</x-link-button>
             </div>
             <div class="bg-white p-6 overflow-hidden shadow-sm sm:rounded-lg">
 
@@ -19,7 +21,7 @@
                 </h2>
                 <p class="mt-4 whitespace-pre-wrap">{{ $aufgabe->text }}</p>
             </div>
-         
+
 
         </div>
     </div>
