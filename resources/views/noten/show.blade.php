a9a929814fd9f88ca53514e0e3fb0b366fbbcea0
Abdeslam Anssar <abdeslamanssar@gmail.com>
2025-06-19 23:53:01 +0000

    Die Anpassungen im  show.blad für deletes und update passen

--- resources/views/noten/show.blade.php
+++ resources/views/noten/show.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Notizen
+              {{ !$aufgabe->trashed() ? 'Notizen' : 'Papierkorb' }}
         </h2>
     </x-slot>
 
@@ -10,9 +10,8 @@
 
             <x-alert-success>{{ session('success') }}</x-alert-success>
 
-
+            @if (!$aufgabe->trashed())
             <div class="flex gap-6">
-
                 <x-link-button href="{{ route('aufgabes.edit', $aufgabe) }}" class="ml-auto">Notiz bearbeiten</x-link-button>
                 <form action="{{ route('aufgabes.destroy', $aufgabe) }}" method="POST">
                     @method('delete')
@@ -36,10 +35,39 @@
                         <td><p class="opacity-70 p-3"><strong>Letzte ändern : </strong> {{$aufgabe->updated_at->diffForHumans()}}</p></td>
                     </tr>
                 </table>
-
-
             </div>
 
+            @else
+            <div class="flex gap-6">
+                <form class="ml-auto" action="{{ route('trashed.update', $aufgabe) }}" method="POST">
+                    @method('put')
+                    @csrf
+                    <x-primary-button>Notiz wiederherstellen</x-primary-button>
+                </form>
+
+                {{-- <form action="{{ route('aufgabes.destroy', $aufgabe) }}" method="POST">
+                    @method('delete')
+                    @csrf
+                    <x-primary-button class="bg-red-500 hover:bg-red-600 focus:bg-red-600"
+                    onclick="return confirm('Sind Sie sicher, dass Sie diese Notiz „{{ $aufgabe->title }}“ In Papierkorb möchten?')"
+                    >In Papierkorb</x-primary-button>
+                </form> --}}
+            </div>
+            <div class="bg-white p-6 overflow-hidden shadow-sm sm:rounded-lg">
+
+                <h2 class="font-bold  text-4xl text-indigo-600">
+                  {{ $aufgabe->title }}
+                </h2>
+                <p class="mt-4 whitespace-pre-wrap">{{ $aufgabe->text }}</p>
+            </div>
+            <div class="max-w-20xl">
+                <table>
+                    <tr>
+                        <td><p class="opacity-70 "><strong>Löschen : </strong> {{$aufgabe->deleted_at->diffForHumans()}}</p></td>
+                    </tr>
+                </table>
+            </div>
+            @endif
         </div>
     </div>
 </x-app-layout>
