8491a27654b13f190d6819bc27f365a301b49341
Abdeslam Anssar <abdeslamanssar@gmail.com>
2025-06-19 16:55:29 +0000

    Notiz löschet Button bearbeitet im  In Papierkorb / Changed Button Notiz löschet to In Papierkorb

--- resources/views/noten/show.blade.php
+++ resources/views/noten/show.blade.php
@@ -7,19 +7,19 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            
+
             <x-alert-success>{{ session('success') }}</x-alert-success>
 
 
             <div class="flex gap-6">
-                
+
                 <x-link-button href="{{ route('aufgabes.edit', $aufgabe) }}" class="ml-auto">Notiz bearbeiten</x-link-button>
                 <form action="{{ route('aufgabes.destroy', $aufgabe) }}" method="POST">
                     @method('delete')
                     @csrf
-                    <x-primary-button class="bg-red-500 hover:bg-red-600 focus:bg-red-600" 
-                    onclick="return confirm('Sind Sie sicher, dass Sie diese Notiz „{{ $aufgabe->title }}“ löschen möchten?')"
-                    >Notiz löschen</x-primary-button>
+                    <x-primary-button class="bg-red-500 hover:bg-red-600 focus:bg-red-600"
+                    onclick="return confirm('Sind Sie sicher, dass Sie diese Notiz „{{ $aufgabe->title }}“ In Papierkorb möchten?')"
+                    >In Papierkorb</x-primary-button>
                 </form>
             </div>
             <div class="bg-white p-6 overflow-hidden shadow-sm sm:rounded-lg">
@@ -32,12 +32,12 @@
             <div class="max-w-20xl">
                 <table>
                     <tr>
-                        <td><p class="opacity-70 p-0"><strong>Erstelen : </strong> {{$aufgabe->created_at->diffForHumans()}}</p></td>
-                        <td><p class="opacity-70 p-6"><strong>Letzte ändern :</strong> {{$aufgabe->updated_at->diffForHumans()}}</p></td>
+                        <td><p class="opacity-70 "><strong>Erstelen : </strong> {{$aufgabe->created_at->diffForHumans()}}</p></td>
+                        <td><p class="opacity-70 p-3"><strong>Letzte ändern : </strong> {{$aufgabe->updated_at->diffForHumans()}}</p></td>
                     </tr>
                 </table>
-            
-            
+
+
             </div>
 
         </div>
