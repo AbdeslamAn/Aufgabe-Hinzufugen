f88f8a75d1b7c933c63c25eb9225a7fc4e262925
Abdeslam Anssar <abdeslamanssar@gmail.com>
2025-06-20 16:15:32 +0000

    Route 'delete' hinzufügen / Create Route 'delete'

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,24 @@ require __DIR__.'/auth.php';
 
 Route::resource('aufgabes', AufgabeController::class)->middleware('auth');
 
+
+// Method 1
+/*
 Route::get('/trashed', [TrashedNoteController::class, 'index'])->middleware('auth')->name('trashed.index');
 
 Route::get('/trashed/{aufgabe}', [TrashedNoteController::class, 'show'])->withTrashed()->middleware('auth')->name('trashed.show');
 
 Route::put('/trashed/{aufgabe}', [TrashedNoteController::class, 'update'])->withTrashed()->middleware('auth')->name('trashed.update');
+
+Route::delete('/trashed/{aufgabe}', [TrashedNoteController::class, 'destroy'])->withTrashed()->middleware('auth')->name('trashed.destroy');
+*/
+
+// Or
+
+Route::prefix('/trashed')->name('trashed.')->middleware('auth')->group(function ()
+{
+    Route::get('/', [TrashedNoteController::class, 'index'])->name('trashed.index');
+    Route::get('/{aufgabe}', [TrashedNoteController::class, 'show'])->withTrashed()->name('.show');
+    Route::put('/{aufgabe}', [TrashedNoteController::class, 'update'])->withTrashed()->name('.update');
+    Route::delete('/{aufgabe}', [TrashedNoteController::class, 'destroy'])->withTrashed()->name('.destroy');
+});
